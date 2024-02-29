9677e2a18ff03e1c0645c2ded5aaa55e86aff3bf
rohithtelukuntla <telukuntlarohith1995@gmail.com>
2024-02-29 21:05:18 +0530

    .

--- Webdriver_project/test/specs/firstDemo.js
+++ Webdriver_project/test/specs/firstDemo.js
@@ -3,8 +3,7 @@ describe("first demo",function(){
        browser.url("https://www.google.com")
        await browser.pause(5000)
        await $('[name="q"]').setValue("webdriverio")
-
-    //    await $('[name="btnK"]').click()
+      //  await $('[name="btnK"]').click()
       //  await $('[type="submit"]').click();
        browser.keys('Enter')
       //  await browser.pause(30000)
